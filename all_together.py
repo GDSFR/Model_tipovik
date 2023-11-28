8ee221cc597388f4840deb72275bff9ae5e518aa
GDSFR <lelikov-ilya@inbox.ru>
2023-11-28 21:54:53 +0300

    ---simp2

--- all_together.py
+++ all_together.py
@@ -49,23 +49,6 @@ def to_plot(way, color, axes, x_pos, y_pos):
         # plt.plot(line)
 
 
-def simp_max(hills):
-    Fmax_val = input_F(hills[0][0], hills[0][1])
-    global Fstep
-    for i in hills:
-        Fstep = input_F(i[0], i[1])
-        if Fstep > Fmax_val:
-            Fmax = i
-            return Fmax
-    return hills[0]
-
-
-def hills_calc(x, y, a):
-    res = [[], [], []]
-    return [[round(x - a / 2, 4), round(y - 0.29 * a, 4)], [round(x + a / 2, 4), round(y - 0.29 * a, 4)],
-            [round(x, 4), round(y + 0.58 * a, 4)]]
-
-
 def simplex_method(start, a, e, axes):
     x_cur = start[0]
     y_cur = start[1]
@@ -211,6 +194,82 @@ def Gauss_Zeudel(start, step_x, step_y, e, axes):
     print(way)
     to_plot(way, 'b', axes, -15, 20)
 
+def simp_max(hills):
+    hill_max_val = input_F(hills[0][0], hills[0][1])
+    global Fstep
+    global hill_max
+    for i in hills:
+        Fstep = input_F(i[0], i[1])
+        if Fstep > hill_max_val:
+            hill_max = i
+    return hill_max
+
+
+def hills_calc(x, y, a):
+    res = [[], [], []]
+    return [[round(x - a / 2, 4), round(y - 0.29 * a, 4)], [round(x + a / 2, 4), round(y - 0.29 * a, 4)],
+            [round(x, 4), round(y + 0.58 * a, 4)]]
+
+
+def simp_min(hills):
+    hill_min_val = input_F(hills[0][0], hills[0][1])
+    global Fstep
+    hill_min = 0
+    for i in hills:
+        Fstep = input_F(i[0], i[1])
+        if Fstep <= hill_min_val:
+            hill_min = i
+    return hill_min
+
+def Nelder_Mead(start, a, e, axes):
+    x_cur = start[0]
+    y_cur = start[1]
+    way = [[x_cur, y_cur, input_F(x_cur, y_cur)]]
+    global F_step
+    last_hills2 = [[[0], [0], [0]], [[0], [0], [0]]]
+    global c
+    hills = hills_calc(x_cur, y_cur, a)
+    for i in range(len(hills)):
+        hills[i].append(round(input_F(hills[i][0], hills[i][1]), 4))
+        way.append(hills[i])
+    while a > e:
+        print(hills)
+        Fmax = simp_max(hills)
+        Fmin = simp_min(hills)
+        for i in range(len(hills)):
+            if hills[i] == Fmax:
+                last_hills2.append(hills.copy())
+                last_hills2.remove(last_hills2[0])
+                hills.remove(hills[i])
+                hills.append(
+                    [round(hills[0][0] + hills[1][0] - Fmax[0], 4), round(hills[0][1] + hills[1][1] - Fmax[1], 1)])
+                break
+        for i in range(len(hills)):
+            if hills[i] == Fmin:
+                hills.remove(hills[i])
+                hills.append(Fmin)
+                hill_stretch = [round(2 * hills[-1][0] - (hills[0][0] + hills[1][0]) / 2, 4),
+                                round(2 * hills[-1][1] - (hills[0][1] + hills[1][1]) / 2, 4),
+                                round(input_F(round(2 * hills[-1][0] - (hills[0][0] + hills[1][0]) / 2, 4),
+                                              round(2 * hills[-1][1] - (hills[0][1] + hills[1][1]) / 2, 4)))]
+                hills.remove(hills[-1])
+                hills.append(hill_stretch)
+                way.append(hills[-1])
+                # way[-1].append(round(input_F(way[-1][0], way[-1][1]), 4))
+                x_cur = hills[-1][0]
+                y_cur = hills[-1][1]
+
+        if ((last_hills2[0][2] == hills[2])
+                or (last_hills2[1][2] == hills[2])):
+            a /= 2
+            hills = hills_calc(x_cur, y_cur, a)
+    print(way)
+    to_plot(way, "purple", axes, -15, 16)
+
+
+def main():
+    return 0
+
 
 if __name__ == "__main__":
     plt.xlim(-15, 30)
@@ -227,7 +286,8 @@ if __name__ == "__main__":
 
     draw_axes(axes)
     draw_func()
-    Gauss_Zeudel(start, step_x, step_y, e, axes)
-    Hooke_Jeeves(start, step_x, step_y, e, axes)
-    simplex_method(start, a, e, axes)
+    # Gauss_Zeudel(start, step_x, step_y, e, axes)
+    # Hooke_Jeeves(start, step_x, step_y, e, axes)
+    # simplex_method(start, a, e, axes)
+    Nelder_Mead(start, a, e, axes)
     plt.show()
