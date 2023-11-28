6c552114ff32544be66f8b486a6f357414bc0499
GDSFR <lelikov-ilya@inbox.ru>
2023-11-28 15:04:53 +0300

    +HJ

--- GZ.py
+++ GZ.py
@@ -26,7 +26,7 @@ def draw_func():
     #    x = np.linspace(-30, 30)
     y = np.linspace(-30, 30, 30000)
     f_vals = [25, 50, 100, 200]
-    plt.text(3, 18, f"Значения построенных ф-ций:{f_vals}")
+    plt.text(3, -30, f"Значения построенных ф-ций:{f_vals}")
     for i in f_vals:
         x_p = - y + np.sqrt(i - (y + 6) ** 2)
         x_n = - y - np.sqrt(i - (y + 6) ** 2)
@@ -35,28 +35,116 @@ def draw_func():
         plt.plot(x_n, y, 'g')
 
 
-def GZ_F(x, y):
+def input_F(x, y):
     return (x + y) ** 2 + (y + 6) ** 2
 
 
-def Gauss_Zeudel(axes):
-    e = 0.2
-    start = [10, 8]
+def to_plot(way, color, axes, x_pos, y_pos):
+    plt.text(x_pos, y_pos, f"Начало: {way[0]} Конец: {way[-1]} Число шагов: {len(way)}")
+    for i in range(len(way)):
+        plt.scatter(way[i][0], way[i][1], s=10, c=color)
+        if i != 0:
+            line = plt.Line2D([way[i - 1][0], way[i][0]], [way[i - 1][1], way[i][1]], c=color)
+            axes.add_line(line)
+        # plt.plot(line)
+
+
+def simp_min(hills):
+    Fmin = input_F(hills[0][1], hills[0][2])
+    global Fstep
+    for i in hills:
+        Fstep = input_F(i[0], i[1])
+        if Fstep < Fmin:
+            Fmin = Fstep
+    return Fmin
+
+def simplex_method(start, a, e, axes):
     x_cur = start[0]
     y_cur = start[1]
     way = [[x_cur, y_cur]]
-    step_x = 2;
-    step_y = 2;
     global F_min
     global F_step
-    F_min = GZ_F(x_cur, y_cur)
+    F_min = input_F(x_cur, y_cur)
+    hills = [[x_cur - a / 2, y_cur - 0.29 * a], [x_cur + a / 2, y_cur - 0.29 * a], [x_cur, y_cur + 0.58 * a]]
+
+    for i in range(len(hills)):
+        way.append(hills[i])
+
+    simp_min(hills)
+
+    return 0
+
+
+def Hooke_Jeeves(start, step_x, step_y, e, axes):
+    x_cur = start[0]
+    y_cur = start[1]
+    way = [[x_cur, y_cur]]
+    global F_min
+    global F_step
+    F_min = input_F(x_cur, y_cur)
 
     while step_x > e or step_y > e:
         x_cur -= step_x
-        F_step = GZ_F(x_cur, y_cur)
+        y_cur -= step_y
+        F_step = input_F(x_cur, y_cur)
         if F_min > F_step:
-            line = plt.Line2D([x_cur + step_x, x_cur], [y_cur, y_cur], color="red")
-            axes.add_line(line)
+            way.append([x_cur, y_cur])
+            F_min = F_step
+            continue
+        else:
+            x_cur += step_x
+            y_cur += step_y
+
+        x_cur -= step_x
+        y_cur += step_y
+        F_step = input_F(x_cur, y_cur)
+        if F_min > F_step:
+            way.append([x_cur, y_cur])
+            F_min = F_step
+            continue
+        else:
+            x_cur += step_x
+            y_cur -= step_y
+
+        x_cur += step_x
+        y_cur -= step_y
+        F_step = input_F(x_cur, y_cur)
+        if F_min > F_step:
+            way.append([x_cur, y_cur])
+            F_min = F_step
+            continue
+        else:
+            x_cur -= step_x
+            y_cur += step_y
+
+        x_cur += step_x
+        y_cur += step_y
+        F_step = input_F(x_cur, y_cur)
+        if F_min > F_step:
+            way.append([x_cur, y_cur])
+            F_min = F_step
+            continue
+        else:
+            x_cur -= step_x
+            y_cur -= step_y
+
+        step_x /= 2
+        step_y /= 2
+    to_plot(way, 'y', axes, -15, 18)
+
+
+def Gauss_Zeudel(start, step_x, step_y, e, axes):
+    x_cur = start[0]
+    y_cur = start[1]
+    way = [[x_cur, y_cur]]
+    global F_min
+    global F_step
+    F_min = input_F(x_cur, y_cur)
+
+    while step_x > e or step_y > e:
+        x_cur -= step_x
+        F_step = input_F(x_cur, y_cur)
+        if F_min > F_step:
             way.append([x_cur, y_cur])
             F_min = F_step
             continue
@@ -64,10 +152,8 @@ def Gauss_Zeudel(axes):
             x_cur += step_x
 
         y_cur -= step_y
-        F_step = GZ_F(x_cur, y_cur)
+        F_step = input_F(x_cur, y_cur)
         if F_min > F_step:
-            line = plt.Line2D([x_cur, x_cur], [y_cur + step_y, y_cur], color="red")
-            axes.add_line(line)
             way.append([x_cur, y_cur])
             F_min = F_step
             continue
@@ -75,10 +161,8 @@ def Gauss_Zeudel(axes):
             y_cur += step_y
 
         x_cur += step_x
-        F_step = GZ_F(x_cur, y_cur)
+        F_step = input_F(x_cur, y_cur)
         if F_min > F_step:
-            line = plt.Line2D([x_cur - step_x, x_cur], [y_cur, y_cur], color="red")
-            axes.add_line(line)
             way.append([x_cur, y_cur])
             F_min = F_step
             continue
@@ -86,10 +170,8 @@ def Gauss_Zeudel(axes):
             x_cur -= step_x
 
         y_cur += step_y
-        F_step = GZ_F(x_cur, y_cur)
+        F_step = input_F(x_cur, y_cur)
         if F_min > F_step:
-            line = plt.Line2D([x_cur, x_cur], [y_cur - step_y, y_cur], color="red")
-            axes.add_line(line)
             way.append([x_cur, y_cur])
             F_min = F_step
             continue
@@ -98,8 +180,8 @@ def Gauss_Zeudel(axes):
 
         step_x /= 2
         step_y /= 2
+    to_plot(way, 'b', axes, -15, 20)
 
-    plt.text(-20, 18, f"Начало: {start}\nКонец: {way[-1]}")
 
 if __name__ == "__main__":
     plt.xlim(-15, 30)
@@ -108,7 +190,15 @@ if __name__ == "__main__":
     axes = plt.gca()
     axes.set_aspect("equal")
 
+    e = 0.2
+    a = 2
+    start = [10, 8]
+    step_x = 2
+    step_y = 2
+
     draw_axes(axes)
     draw_func()
-    Gauss_Zeudel(axes)
+    Gauss_Zeudel(start, step_x, step_y, e, axes)
+    Hooke_Jeeves(start, step_x, step_y, e, axes)
+    simplex_method(start, step_x, step_y, e, axes)
     plt.show()
