f785b1fb78bf154e4e66c0833335d4a009b0f735
gdsfr <aaa@aaa.ue>
2023-12-02 14:36:21 +0300

    +++- simp2

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from GZ_method import Gauss_Zeudel
 from HJ_method import Hooke_Jeeves
 from simplex1 import simplex_method
 from simplex2 import simplex_method_Nelder_Mead
+
 if __name__ == "__main__":
     plt.xlim(-15, 30)
     plt.ylim(-25, 15)
@@ -16,15 +17,15 @@ if __name__ == "__main__":
     e = 0.2
     a1 = 2
     a2 = 2
-    start = [10, 8]
+    start = [10, -18]
     step_x = 2
     step_y = 2
 
     draw_axes(axes)
     draw_func()
-    #Gauss_Zeudel(start, step_x, step_y, e, axes)
-    #Hooke_Jeeves(start, step_x, step_y, e, axes)
-    #simplex_method(start, a1, e, axes)
+    # Gauss_Zeudel(start, step_x, step_y, e, axes)
+    # Hooke_Jeeves(start, step_x, step_y, e, axes)
+    # simplex_method(start, a1, e, axes)
     simplex_method_Nelder_Mead(start, a2, e, axes)
 
     plt.show()
--- simplex2.py
+++ simplex2.py
@@ -40,7 +40,6 @@ def calc_hill_1(hills, Fmax):
                 input_F(round(hills[0][0] + hills[1][0] - Fmax[0], 4), round(hills[0][1] + hills[1][1] - Fmax[1], 4)))]
 
 
-
 # return [
 #     [round(, 4), round(, 4), round(, 4)],
 #     [round(, 4), round(, 4), round(, 4)],
@@ -55,23 +54,32 @@ def calc_hill_2(hills, Fmin, a):
                       round(2 * Fmin[1] - (hills[0][1] + hills[1][1]) / 2, 4), ), 4)
     ]
 
-def calc_hill_2red(hills, Fmin, a, k):
-    return [
-        round(k * Fmin[0] - (hills[0][0] + hills[1][0]) / (a * 2), 4),
-        round(k * Fmin[1] - (hills[0][1] + hills[1][1]) / (a * 2), 4),
-        round(input_F(round(k * Fmin[0] - (hills[0][0] + hills[1][0]) / (a * 2), 4),
-                      round(k * Fmin[1] - (hills[0][1] + hills[1][1]) / (a * 2), 4), ), 4)
+
+def calc_hill_2red(hills, Fmin, a, red_c):
+    k = 0.5
+    x_res = round(k * Fmin[0] + (hills[0][0] + hills[1][0]) / (a * 2**red_c), 4)
+    y_res = round(k * Fmin[1] + (hills[0][1] + hills[1][1]) / (a * 2**red_c), 4)
+    f_res = round(input_F(x_res, y_res), 4)
+
+    res = [
+        x_res,
+        y_res,
+        f_res
     ]
+    return res
+
 
 def check_red(hills):
     if hills[2] != simp_min(hills):
         return True
     return False
 
+
 def check_sqrt(hills, e):
     if round(1, 4) < e:
         return True
     return True
+
 
 def simplex_method_Nelder_Mead(start, a, e, axes):
     x_cur = start[0]
@@ -80,6 +88,8 @@ def simplex_method_Nelder_Mead(start, a, e, axes):
     global F_step
     last_hills2 = [[[0], [0], [0]], [[0], [0], [0]]]
     global c
+    global red_c
+    red_c = 0
     c = 0
     k = 0.5
     hills = hills_calc(x_cur, y_cur, a)
@@ -103,17 +113,22 @@ def simplex_method_Nelder_Mead(start, a, e, axes):
         for i in hills:
             if i == Fmin:
                 hill_min = calc_hill_2(hills, Fmin, a)
-                if hill_min[2] < hills[2][2] or hill_min[2] < hills[1][2] or hill_min[2] < hills[0][2]:
+                print(f"hill_min: \n{hill_min}")
+                if hill_min[2] < hills[2][2]:
                     hills.remove(hills[2])
                     hills.append(hill_min)
+                else:
                     break
-                #hills.remove(i)
-                #hills.append(Fmin)
-                hill_min_red = calc_hill_2red(hills, hills[2], a, k)
+                if hill_min[2] < hills[2][2] or hill_min[2] < hills[1][2] or hill_min[2] < hills[0][2]:
+                    break
+                # hills.remove(i)
+                # hills.append(Fmin)
+                red_c += 1
+                hill_min_red = calc_hill_2red(hills, hills[2], a, red_c)
+                print(f"hill_red: \n{hill_min_red}")
                 if hill_min_red[2] < hills[2][2]:
                     hills.remove(hills[2])
                     hills.append(hill_min_red)
-                print(f"hill_red: \n{hill_min_red}")
                 break
 
         way.append(hills[-1])
@@ -124,7 +139,6 @@ def simplex_method_Nelder_Mead(start, a, e, axes):
             if input_F(i[0], i[1]) == 0:
                 a = e
         c += 1
-
 
     print(way)
     to_plot(way, 'purple', axes, -15, 16)
